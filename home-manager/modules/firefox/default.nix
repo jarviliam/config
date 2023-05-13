ad77b9dfc974f867b7ca2717e8663c952117762f
Liam Jarvis <jarviliam@gmail.com>
2023-05-13 14:20:05 +0900

    add firefox

--- home-manager/modules/firefox/default.nix
+++ home-manager/modules/firefox/default.nix
@@ -1,5 +1,44 @@
-{lib,pkg,...}:{
+{lib,pkgs,...}:{
   programs.firefox = {
       enable = true;
+      profiles.default = {
+        # bookmarks = [{}]
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          privacy-badger
+          bitwarden
+          bypass-paywalls-clean
+          vimium
+          reddit-enhancement-suite
+        ];
+        search = {
+          engines = {
+            "Nix Packages" = {
+              urls = [{
+                template = "https://search.nixos.org/packages";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+
+            "NixOS Wiki" = {
+              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@nw" ];
+            };
+
+            "Bing".metaData.hidden = true;
+            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          };
+          order = ["DuckDuckGo" "Google"];
+          };
+        userChrome = builtins.readFile("./userChrome.css");
+        };
+
     };
   }
