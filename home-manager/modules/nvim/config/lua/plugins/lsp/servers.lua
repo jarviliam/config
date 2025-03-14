2954cf383297ead5a56bf3701cb9c625f976047c
Liam Jarvis <jarviliam@gmail.com>
2025-03-14 16:14:04 +0900

    feat: ignore node modules

--- home-manager/modules/nvim/config/lua/plugins/lsp/servers.lua
+++ home-manager/modules/nvim/config/lua/plugins/lsp/servers.lua
@@ -88,7 +88,7 @@ return {
         usePlaceholders = true,
         completeUnimported = true,
         staticcheck = true,
-        directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+        directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-**/node_modules" },
         semanticTokens = true,
       },
     },
