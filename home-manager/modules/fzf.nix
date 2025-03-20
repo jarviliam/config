f86cf92f543ef85f97366e596d46524ffda64c96
Liam Jarvis <jarviliam@gmail.com>
2025-03-20 21:42:22 +0900

    ✨ feat(packages): Add gruvbox to starship
    Changelog: feature

--- home-manager/modules/fzf.nix
+++ home-manager/modules/fzf.nix
@@ -1,36 +1,36 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.fzf = {
     enable = true;
     fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
     changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
     colors = {
-      "fg" = "#848d97";
-      "bg" = "#30363d";
-      "hl" = "#ffffff";
-      "fg+" = "#e6edf3";
-      "bg+" = "#313f50";
-      "hl+" = "#ffa657";
-      "info" = "#d29922";
-      "prompt" = "#2f81f7";
-      "pointer" = "#a371f7";
-      "marker" = "#3fb950";
-      "spinner" = "#6e7681";
-      "header" = "#495058";
+      "fg" = "#fbf1c7"; # light foreground
+      "bg" = "#3c3836"; # dark background
+      "hl" = "#fabd2f"; # highlight
+      "fg+" = "#ebdbb2"; # brighter foreground
+      "bg+" = "#282828"; # slightly lighter background
+      "hl+" = "#fe8019"; # brighter highlight
+      "info" = "#8ec07c"; # info text
+      "prompt" = "#d3869b"; # prompt text
+      "pointer" = "#b8bb26"; # pointer
+      "marker" = "#83a598"; # marker
+      "spinner" = "#d65d0e"; # spinner
+      "header" = "#d79921"; # header text
     };
     # colors = {
-    #   "bg+" = "#414559";
-    #   bg = "#303446";
-    #   spinner = "#f2d5cf";
-    #   hl = "#e78284";
-    #   fg = "#c6d0f5";
-    #   header = "#e78284";
-    #   info = "#ca9ee6";
-    #   pointer = "#f2d5cf";
-    #   marker = "#f2d5cf";
-    #   "fg+" = "#c6d0f5";
-    #   prompt = "#ca9ee6";
-    #   "hl+" = "#e78284";
+    #   "fg" = "#848d97";
+    #   "bg" = "#30363d";
+    #   "hl" = "#ffffff";
+    #   "fg+" = "#e6edf3";
+    #   "bg+" = "#313f50";
+    #   "hl+" = "#ffa657";
+    #   "info" = "#d29922";
+    #   "prompt" = "#2f81f7";
+    #   "pointer" = "#a371f7";
+    #   "marker" = "#3fb950";
+    #   "spinner" = "#6e7681";
+    #   "header" = "#495058";
     # };
   };
 }
--- home-manager/modules/nvim/config/lua/plugins/init.lua
+++ home-manager/modules/nvim/config/lua/plugins/init.lua
@@ -83,6 +83,18 @@ return {
   },
   { "tpope/vim-abolish", command = "S" },
   { "sainnhe/everforest" },
-  { "sainnhe/gruvbox-material", lazy = false },
+  {
+    "sainnhe/gruvbox-material",
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_foreground = "material"
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_background = "soft" -- soft, medium, hard
+      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_statusline_style = "material"
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+    end,
+    lazy = true,
+  },
   { "projekt0n/github-nvim-theme" },
 }
