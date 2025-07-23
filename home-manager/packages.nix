cf41a20038fd71442abd6dedd0beb2d3fcc45472
Liam Jarvis <jarviliam@gmail.com>
2025-07-23 09:29:52 +0900

    chore: fix python def

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -15,8 +15,7 @@ let
     python312
     codespell
     pre-commit
-    poetry.withPlugins
-    (ps: with ps; [ poetry-plugin-export ])
+    (poetry.withPlugins [ poetry-plugin-export ])
     pyright
     basedpyright
     ruff
